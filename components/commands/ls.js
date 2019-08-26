27241d05460e8ec7cd1524d40a5334c6e1a398f8
Leon Machens <leon.machens@gmail.com>
2019-08-26 13:33:27 +0200

    add ls styles

--- components/commands/ls.js
+++ components/commands/ls.js
@@ -1,6 +1,9 @@
 import { writeCommandOutput } from "../cli.js";
 
 export function ls(props, level) {
+  if (level.childs.length === 0) {
+    return;
+  }
   const listItems = level.childs
     .map(child => `<li>${child.name}</li>`)
     .join("\n");
